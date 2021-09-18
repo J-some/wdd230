e4a3240dcb54ff7d9318f6aebe45548b88515a07
j-some <somearg@byui.edu>
2021-09-18 15:18:23 -0300

    week 1

---
+++ jss/date.js
@@ -0,0 +1,6 @@
+try {
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.querySelector('#currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+  } catch (e) {
+    alert('Error with code or your browser does not support Locale');
+  }
