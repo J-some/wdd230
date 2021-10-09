5f0785f94f94edffe68f07ce733cec96b0519db7
j-some <somearg@byui.edu>
2021-10-09 20:01:09 -0300

    updated

--- lesson4/jss/script.js
+++ lesson4/jss/script.js
@@ -1,16 +1,3 @@
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
-
-try {
-  const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  };
-  document.querySelector("#currentdate").textContent =
-    new Date().toLocaleDateString("en-US", options);
-} catch (e) {
-  alert("Error with code or your browser does not support Locale");
-}
--- lesson4/jss/scriptDate.js
+++ lesson4/jss/scriptDate.js
@@ -1,11 +1,11 @@
 try {
-  const options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
     year: "numeric",
   };
-  document.querySelector("#currentdate").textContent =
+  document.getElementById("currentdate").textContent =
     new Date().toLocaleDateString("en-US", options);
 } catch (e) {
   alert("Error with code or your browser does not support Locale");
