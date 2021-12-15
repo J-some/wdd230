02fcee21b7f1ceb4b89e5800a7af97b351fd90c1
j-some <somearg@byui.edu>
2021-12-15 19:15:59 -0300

    updated

--- js/banner.js
+++
@@ -1,8 +0,0 @@
-let date = new Date();
-let weekday = date.getDay();
-
-if (weekday == "5") {
-  document.getElementById("bann").style.display = "block";
-} else {
-  document.getElementById("bann").style.display = "none";
-}
